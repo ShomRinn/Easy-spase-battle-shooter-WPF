ccfe4eba66c70cf44ddd8db3100cd48c992562fe
Georgi Bachilo <goshasbachilo@gmail.com>
2022-06-06 18:43:21 +0300

    Add project files.

---
+++ Easy spase battle shooter WPF/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Windows.Threading;
+
+namespace Easy_spase_battle_shooter_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        DispatcherTimer gameTimer = new DispatcherTimer();
+        bool MoveRight, MoveLeft;
+        List<Rectangle> itemRemover = new List<Rectangle>();
+
+        Random rand = new Random();
+
+        int EnemySpriteCounter = 0;
+        int EnemyCounter = 100;
+        int PlayerSpeed = 10;
+        int limit = 50;
+        int score = 0;
+        int damage = 0;
+        int enemySpeed = 10;
+
+        Rect PlayerHitBox;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            gameTimer.Interval = TimeSpan.FromMilliseconds(20);
+            gameTimer.Tick +=GameLoop;
+            gameTimer.Start();
+
+            MyCanvas.Focus();
+            ImageBrush bg = new ImageBrush();
+            bg.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/purple.png"));
+            bg.TileMode = TileMode.Tile;
+            bg.Viewport = new Rect(0, 0, 0.15, 0.15);
+            bg.ViewportUnits = BrushMappingMode.RelativeToBoundingBox;
+            MyCanvas.Background = bg;
+
+        }
+
+        private void GameLoop(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void OnKeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void MakeEnemies()
+        {
+
+        }
+    }
+}
