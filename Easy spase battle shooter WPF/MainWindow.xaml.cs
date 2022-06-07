a83e3951f48765167a55c90dc1f28b3e4b45aea9
Georgi Bachilo <goshasbachilo@gmail.com>
2022-06-07 19:18:10 +0300

    add movement and pew-pew

--- Easy spase battle shooter WPF/MainWindow.xaml.cs
+++ Easy spase battle shooter WPF/MainWindow.xaml.cs
@@ -65,21 +65,143 @@ namespace Easy_spase_battle_shooter_WPF
 
         private void GameLoop(object sender, EventArgs e)
         {
-          
+            PlayerHitBox = new Rect(Canvas.GetLeft(player), Canvas.GetTop(player), player.Width, player.Height);
+
+            EnemyCounter += 1;
+
+            ScoreText.Content = "Score" + score;
+            DamageText.Content = "danage" + damage;
+
+            if (EnemyCounter < 0)
+            {
+                MakeEnemies();
+                EnemyCounter = limit;
+            }
+
+            if (MoveLeft == true && Canvas.GetLeft(player) > 0)
+            {
+
+                Canvas.SetLeft(player, Canvas.GetLeft(player) - PlayerSpeed);
+
+            }
+
+            if (MoveRight == true && Canvas.GetLeft(player) + 90 < Application.Current.MainWindow.Width)
+            {
+
+                Canvas.SetLeft(player, Canvas.GetLeft(player) + PlayerSpeed);
+
+            }
+
+
+            foreach (var x in MyCanvas.Children.OfType<Rectangle>())
+            {
+
+                if (x is Rectangle && (string)x.Tag == "bullet")
+                {
+                    Canvas.SetTop(x, Canvas.GetTop(x)-20);
+                    Rect bulletHitBox = new Rect(Canvas.GetLeft(x), Canvas.GetTop(x), x.Width, x.Height);
+
+                    if (Canvas.GetTop(x) < 10)
+                    {
+                        itemRemover.Add(x);
+                    }
+
+                }
+
+                if (x is Rectangle && (string)x.Tag == "enemy")
+                {
+                    Canvas.SetTop(x, Canvas.GetTop(x) + enemySpeed);
+                }
+
+
+            }
+
         }
 
-        private void OnKeyDown(object sender, KeyEventArgs e)
-        {
+       
 
+private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Left)
+            {
+                MoveLeft = true;
+            }
+            if (e.Key == Key.Right)
+            {
+                MoveRight = true;
+            }
         }
 
         private void OnKeyUp(object sender, KeyEventArgs e)
         {
+            if (e.Key == Key.Left)
+            {
+                MoveLeft = false;
+            }
+            if (e.Key == Key.Right)
+            {
+                MoveRight = false;
+            }
+
+            if (e.Key == Key.Space)
+            {
+                Rectangle newBullet = new Rectangle
+                {
+                    Tag = "bullet",
+                    Height = 20,
+                    Width = 5,
+                    Fill = Brushes.White,
+                    Stroke = Brushes.Red
+                };
+
+                Canvas.SetLeft(newBullet, Canvas.GetLeft(player) + player.Width / 2);
+                Canvas.SetTop(newBullet, Canvas.GetTop(player) - newBullet.Height);
+
+                MyCanvas.Children.Add(newBullet);
+
+            }
 
         }
 
-        private void MakeEnemies()
-        {
+
+        private void MakeEnemies(){
+
+            ImageBrush EnemySprite = new ImageBrush();
+
+            EnemySpriteCounter = rand.Next(1, 5);
+
+            switch (EnemySpriteCounter){
+
+                case 1:
+                    EnemySprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/1.png"));
+                        break;
+                case 2:
+                    EnemySprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/2.png"));
+                    break;
+                case 3:
+                    EnemySprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/3.png"));
+                    break;
+                case 4:
+                    EnemySprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/4.png"));
+                    break;
+                case 5:
+                    EnemySprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/5.png"));
+                    break;
+
+            }
+
+            Rectangle newEnemy = new Rectangle
+            {
+
+                Tag = "enemy",
+                Height = 50,
+                Width = 56,
+                Fill = EnemySprite
+            };
+
+            Canvas.SetTop(newEnemy, -100);
+            Canvas.SetLeft(newEnemy, rand.Next(30, 430));
+            MyCanvas.Children.Add(newEnemy);
 
         }
     }
